08e55fe721fcc6d528f984b46b53091742f7f8df
teja <tejsshiva52@gmail.com>
2023-09-24 14:31:57 -0400

    modified c file to js file & completed the task.

---
+++ addTwoNum.js
@@ -0,0 +1,4 @@
+//write a code to add two integer nubers here
+const num1 = prompt("Enter your 1st number :- ");
+const num2 = prompt("Enter your 2nd number :- ");
+alert("your sum is "+(num1+num2));
